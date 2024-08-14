a77d209c1c64c5f1dfb48344976817344b421c93
Sidak <magicalsidak@gmail.com>
2024-08-13 22:57:28 -0400

    it works

--- web-dev/app/api/translate/route.js
+++ web-dev/app/api/translate/route.js
@@ -1,28 +1,28 @@
-// import { Translate } from "@google-cloud/translate";
+import { Translate } from "@google-cloud/translate";
 
 export const POST = async (req) => {
-  const translate = {
+  const translate = new Translate({
     projectId: "my_project",
     keyFilename: "path/to/your/keyfile.json",
-  };
+  });
 
   const { word, language } = await req.json();
 
-  // try {
-  //   const [translation] = await translate.translate(word, language);
-  //   return new Response(JSON.stringify({ translation }), {
-  //     status: 200,
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   });
-  // } catch (err) {
-  //   console.error("ERROR:", err);
-  //   return new Response(JSON.stringify({ error: "Translation failed" }), {
-  //     status: 500,
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   });
-  // }
+  try {
+    const [translation] = await translate.translate(word, language);
+    return new Response(JSON.stringify({ translation }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (err) {
+    console.error("ERROR:", err);
+    return new Response(JSON.stringify({ error: "Translation failed" }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 };
